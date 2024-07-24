5a24503d8ad3692d69c14e60541dfb3d113180ca
Duy Hung <daoduyhung98@gmailcom>
2024-07-24 14:10:25 +0700

    Initial commit

--- check_erro/models/abc.py
+++ check_erro/models/abc.py
@@ -6,10 +6,12 @@ import requests
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-url = 'https://hscvlqd.dienbien.gov.vn'
 def check_website_status(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
     try:
-        response = requests.get(url, verify=False)
+        response = requests.get(url, headers=headers,verify=False)
         if response.status_code == 200:
             print(f"Website {url} is up and running.")
         else:
@@ -18,4 +20,4 @@ def check_website_status(url):
         print(f"Website {url} is down. Error: {e}")
 
 # Example usage
-check_website_status("https://hscvlqd.dienbien.gov.vn")
+check_website_status("https://ict.dienbien.gov.vn/")
--- check_erro/models/check_web.py
+++ check_erro/models/check_web.py
@@ -232,13 +232,13 @@ class WebsiteStatus(models.Model):
                 #     record.bot_send_tele.send_message(message, parse_mode='HTML')
 
     def check_fast(self):
-        # headers = {
-        #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        # }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
         # session = requests.Session()
         def fetch_status(record):
             try:
-                response = requests.get(record.name, verify=False)
+                response = requests.get(record.name, headers=headers,verify=False)
                 record.status_code = str(response.status_code)
                 if response.status_code == 200:
                     record.qty_links = 1
