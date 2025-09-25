0eba77794b47bff43bd0d2c6d06fc27cab611e24
Duy Hung <daoduyhung98@gmailcom>
2025-09-25 08:29:05 +0700

    update ip

---
+++ info_sql/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "info_sql",
+    'summary': """
+        zalo messager""",
+    'description': """
+        zalo
+    """,
+    'author': "HungDD",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '1.1',
+    'depends': [
+        'check_erro'
+    ],
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+    'license': 'LGPL-3',
+}
---
+++ info_sql/controllers/info_sql.py
@@ -0,0 +1,13 @@
+from odoo import http
+from odoo.http import request
+
+class InfoSQLController(http.Controller):
+
+    @http.route('/info_sql', type='json', auth='public', methods=['POST'], csrf=False)
+    def info_sql(self, **kwargs):
+        # Lấy dữ liệu JSON gửi đến
+        data = request.jsonrequest
+        return {
+            "status": "success",
+            "received": data
+        }
